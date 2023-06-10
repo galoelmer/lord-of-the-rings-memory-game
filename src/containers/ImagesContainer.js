78403f942e7956f5ca5d2e65679dd24fed4ea35e
Elmer Galo <elmer.galo@outlook.com>
2023-06-09 23:08:42 -0400

    build: update app dependencies

--- src/containers/ImagesContainer.js
+++ src/containers/ImagesContainer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo, useContext } from "react";
-import { useTransition, a } from "react-spring";
-import useMeasure from "../hooks/useMeasure";
+import { useTransition, animated } from "@react-spring/web";
+import useMeasure from "react-use-measure";
 import { makeStyles } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { GameContext } from "../context/GameContext";
@@ -10,15 +10,19 @@ const useStyles = makeStyles((theme) => ({
     position: "relative",
     width: "100%",
     height: "100%",
+
     "& > div": {
       position: "absolute",
       filter: "brightness(1.15)",
-      "& > img": {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+
+      "& > div": {
+        position: "relative",
         width: "90%",
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
+        height: "90%",
+        overflow: "hidden",
         borderRadius: 10,
         boxShadow:
           "0 6px 6px rgba(0, 0, 0, 0.4), 2px 8px 8px rgba(0, 0, 0, 0.2)",
@@ -32,28 +36,31 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const ImagesContainer = () => {
-  const mobileSize = useMediaQuery("(max-width:600px)");
+  const [clickedImages, setClickedImages] = useState([]);
+
   const classes = useStyles();
+  const [ref, { width }] = useMeasure();
+  const mobileSize = useMediaQuery("(max-width:600px)");
+
   const {
     scoreDispatch,
     imagesList: { list },
     listDispatch,
   } = useContext(GameContext);
-  const [clickedImages, setClickedImages] = useState([]);
-  const columns = mobileSize ? 3 : 4;
-  const [bind, { width }] = useMeasure();
 
+  const columns = mobileSize ? 3 : 4;
+
+  // Form a grid of stacked items using width & columns
   const [heights, gridItems] = useMemo(() => {
     let heights = new Array(columns).fill(0);
     let gridItems = list.map((child) => {
       const column = heights.indexOf(Math.min(...heights));
-      const xy = [
-        (width / columns) * column,
-        (heights[column] += width / columns) - width / columns,
-      ];
+      const x = (width / columns) * column;
+      const y = (heights[column] += width / columns) - width / columns;
       return {
         ...child,
-        xy,
+        x,
+        y,
         width: width / columns,
         height: width / columns,
       };
@@ -61,10 +68,12 @@ const ImagesContainer = () => {
     return [heights, gridItems];
   }, [columns, list, width]);
 
-  const transitions = useTransition(gridItems, (item) => item.image, {
-    from: ({ xy, width, height }) => ({ xy, width, height, opacity: 0 }),
-    enter: ({ xy, width, height }) => ({ xy, width, height, opacity: 1 }),
-    update: ({ xy, width, height }) => ({ xy, width, height }),
+  // Turn the static grid values into animated transitions, any addition, removal or change will be animated
+  const transitions = useTransition(gridItems, {
+    key: (item) => item.image,
+    from: ({ x, y, width, height }) => ({ x, y, width, height, opacity: 0 }),
+    enter: ({ x, y, width, height }) => ({ x, y, width, height, opacity: 1 }),
+    update: ({ x, y, width, height }) => ({ x, y, width, height }),
     leave: { height: 0, opacity: 0 },
     config: { mass: 5, tension: 500, friction: 100 },
     trail: 25,
@@ -77,62 +86,53 @@ const ImagesContainer = () => {
       scoreDispatch({ type: "SET_CHALLENGE_COMPLETE" });
       scoreDispatch({ type: "OPEN_MODAL" });
     }
-    /**
-     *  Cheat console log
-     */
-    // const imagesClicked = list
-    //   .filter((x) => clickedImages.includes(x.key))
-    //   .map((y) => y.name);
-    // console.log(imagesClicked);
   }, [clickedImages, scoreDispatch]);
 
-  /**
-   * Score update after clicking images
-   */
   const handleClick = (key) => {
     if (!clickedImages.includes(key)) {
       setClickedImages((prevState) => [key, ...prevState]);
     } else {
-      // Find character's name with a doubled click
-      // setDoubleClickImage(() => {
-      //   return list.find((item) => item.key === key).name;
-      // });
       const character = list.find((item) => item.key === key);
+
       if (character) {
         scoreDispatch({ type: "SET_CHARACTER_NAME", payload: character.name });
         scoreDispatch({ type: "OPEN_MODAL" });
       }
+
       scoreDispatch({ type: "RESET_SCORE" });
       setClickedImages([]);
     }
+
     listDispatch({ type: "SHUFFLE_LIST_IMAGES" });
   };
 
   return (
-    <>
-      <div
-        {...bind}
-        className={classes.list}
-        style={{ height: Math.max(...heights) }}
-      >
-        {transitions.map(({ item, props: { xy, ...rest }, key }) => {
-          return (
-            <a.div
-              key={key}
+    <div
+      ref={ref}
+      className={classes.list}
+      style={{ height: Math.max(...heights) }}
+    >
+      {transitions((style, item) => {
+        return (
+          <animated.div
+            key={item.key}
+            style={{
+              ...style,
+              willChange: "transform, opacity, width, height",
+            }}
+            onClick={() => handleClick(item.key)}
+          >
+            <div
               style={{
-                transform: xy.interpolate(
-                  (x, y) => `translate3d(${x}px,${y}px,0)`
-                ),
-                ...rest,
+                background: `url(${item.image})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center center",
               }}
-              onClick={() => handleClick(item.key)}
-            >
-              <img src={item.image} alt={item.image} />
-            </a.div>
-          );
-        })}
-      </div>
-    </>
+            />
+          </animated.div>
+        );
+      })}
+    </div>
   );
 };
 
